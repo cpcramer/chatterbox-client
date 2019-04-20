c4f96e8d8dc8a6badb2706dafa0baa776bf5a156
Chadcramer <chadpcramer@gmail.com>
2019-04-19 19:40:18 -0500

    progress through chatterbox day 1

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -11,6 +11,11 @@ var FormView = {
     event.preventDefault();
     
     console.log('click!');
+    let userName = App.username;
+    let myTweet = $('#message').val();
+    let newTweet = $("<div class='chat'><div class='username'><h3>" + userName + "</h3></div><div class='text'><h5> " + myTweet + "</h5></div></div>")
+    // {class: 'chat'}
+    $('#chats').prepend(newTweet);
   },
 
   setStatus: function(active) {
